7c5909509fa1d1c73f70acb1abd1ea0b07f47e74
Matthew Brian <matt@brians.com.au>
2023-08-05 17:05:39 +1000

    Create the Vulkan Instance

--- main.cpp
+++ main.cpp
@@ -1,35 +1,108 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
 
-int main() {
-    glfwInit();
+#define WIDTH 800
+#define HEIGHT 600
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+class Application {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    GLFWwindow* window;
+    VkInstance instance;
 
-    glfwTerminate();
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Caplan", nullptr, nullptr);
+    }
 
-    return 0;
+    void initVulkan() {
+        createInstance();
+    }
+
+    void createInstance() {
+        auto appInfo = VkApplicationInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.pNext = nullptr;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwRequiredExtensions;
+        glfwRequiredExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        auto createInfo = VkInstanceCreateInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwRequiredExtensions;
+        createInfo.enabledLayerCount = 0;
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            // TODO: Remove error throwing once have a good idea of starting code structure
+            throw std::runtime_error("Failed to create a Vulkan instance");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        auto availableExtensions = std::vector<VkExtensionProperties>(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableExtensions.data());
+
+        // TODO: Should use a trie so not spam checking all available extensions?
+        for (auto i = 0; i < glfwExtensionCount; ++i) {
+            auto extensionAvailable = false;
+            for (const auto& extension : availableExtensions) {
+                if (strcmp(extension.extensionName, glfwRequiredExtensions[i])) {
+                    extensionAvailable = true;
+                    break;
+                }
+            }
+
+            if (!extensionAvailable) {
+                throw std::runtime_error("GLFW Required Extension " + std::string(glfwRequiredExtensions[i]) + " is not available");
+            }
+            std::cout << "Found " << glfwRequiredExtensions[i] << "!" << std::endl;
+        }
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    Application app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
